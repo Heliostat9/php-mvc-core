d8ebe7cd03360b5c9fb50c0c3c8f6babafb38938
Heliostat9 <heliostat@inbox.ru>
2021-08-28 11:56:31 +0300

    initial commit

---
+++ Controller.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace app\core;
+
+
+use app\core\middlewares\BaseMiddleware;
+
+class Controller
+{
+    /**
+     * @var BaseMiddleware[]
+     */
+    protected array $middlewares = [];
+    public string $layout = 'main';
+    public string $action = '';
+
+    public function setLayout($layout) {
+        $this->layout = $layout;
+    }
+
+    public function render($view, $params = []) {
+        return Application::$app->view->renderView($view, $params);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    /**
+     * @return BaseMiddleware[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+
+}
